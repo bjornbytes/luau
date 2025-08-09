da5dbebe9c645dd4d61e117980b35d94b94f083d
bjorn <mail@bjornbyt.es>
2025-08-09 10:53:50 -0700

    Move some changes into lauxlib.h; rm LUA_VECTOR4;

--- VM/include/lauxlib.h
+++ VM/include/lauxlib.h
@@ -1,1 +1,9 @@
 #include "lualib.h"
+
+#define LUA_VERSION_NUM 501
+
+#define luaL_checkint(L, i) ((int) luaL_checkinteger(L, i))
+#define luaL_optint(L, i, d) ((int) luaL_opinteger(L, i, d))
+
+#define luaL_ref(L, idx) lua_ref(L, -1), lua_pop(L, 1)
+#define luaL_unref(L, idx, ref) lua_unref(L, ref)
